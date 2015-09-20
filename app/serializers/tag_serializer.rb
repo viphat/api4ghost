874a1329a9c9c810206ea4e99f6ed1ca7fe88e9c
Eddie Yang <viphat@gmail.com>
2015-09-20 08:00:56 +0700

    Remove Database Password

--- app/serializers/tag_serializer.rb
+++ app/serializers/tag_serializer.rb
@@ -1,5 +1,5 @@
 class TagSerializer < ActiveModel::Serializer
-  cache key: 'tags', expires_in: 3.hours
+  # cache key: 'tags', expires_in: 3.hours
   attributes :id, :text, :posts_count, :link, :weight
   # has_many :posts_tags
 end
